6a248be6c2758d350b74e87cf5f01f1cfd8028af
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-08-16 13:17:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+  let wrapper= document.querySelector
